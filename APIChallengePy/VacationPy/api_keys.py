02b6963521e9c2ea748047c1a163a0b60bf8e435
carlos-hernandezortiz <carlos.hdezor@gmail.com>
2021-04-28 23:13:25 -0500

    Added solutions

---
+++ APIChallengePy/VacationPy/api_keys.py
@@ -0,0 +1,3 @@
+# Google API Key
+g_key = "AIzaSyD5DVqYwmdVe0b9EZhUwUt4pqJekeUrU98"
+weather_api_key = "22f0447e98c52b30003ad584f457d055"
---
+++ APIChallengePy/WeatherPy/api_keys.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "22f0447e98c52b30003ad584f457d055"
